a6327f630c4ecbcb775c76d2994accf43326560e
bharshbarger <bharshbarger@users.noreply.github.com>
2017-09-27 15:08:51 -0500

    Update get_external_ip.py

--- get_external_ip.py
+++ get_external_ip.py
@@ -4,6 +4,7 @@ import sys
 try:
     external_ip = requests.get('http://ifconfig.co', headers = self.requests_useragent).content
     return external_ip.strip('\n')
+    print(external_ip)
 except requests.exceptions.RequestException as e:
     print(e)
     sys.exit(1)
