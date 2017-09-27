327271e56c22c2b7b18f2ea19298d993484399bb
bharshbarger <bharshbarger@users.noreply.github.com>
2017-09-27 15:06:36 -0500

    Update write_file.py

--- write_file.py
+++ write_file.py
@@ -1,5 +1,10 @@
+import sys
 
 def write_file(self, content, filename):
-
-    with open(filename, 'w') as file:
-        file.writelines(content)
+    try:
+        with open(filename, 'w') as file:
+            file.writelines(content)
+            file.close()
+    except Exception as e:
+        print(e)
+        sys.exit(1)
