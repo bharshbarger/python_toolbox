b41cd5b4e43022dc42ee62be07cf75c90a5f2b96
bharshbarger <bharshbarger@users.noreply.github.com>
2017-09-26 22:59:44 -0500

    Create yes_no.py

---
+++ yes_no.py
@@ -0,0 +1,12 @@
+def yes_no(self, answer):
+    yes = set(['yes','y', 'ye', ''])
+    no = set(['no','n'])
+
+    while True:
+        choice = raw_input(answer).lower()
+        if choice in yes:
+           return True
+        elif choice in no:
+           return False
+        else:
+           print "Please respond with 'yes' or 'no'\n"
