9659d45be6b5193f7d84cc81a59daf076e2eb5fe
bharshbarger <bharshbarger@users.noreply.github.com>
2017-09-27 02:08:38 -0500

    Update start_simple_http_server.py

--- start_simple_http_server.py
+++ start_simple_http_server.py
@@ -1,36 +1,13 @@
-import subprocess
-import sys
-import urllib2
-from time import sleep
+import http.server
+import socketserver
+import os
 
-def serv():
-	null = open('/dev/null', 'w')
+PORT = 8000
 
-	p = subprocess.Popen(
-		[sys.executable, '-m', 'SimpleHTTPServer', '8124'], 
-		cwd='/tmp',
-		stdout=null,
-		stderr=null,		
-	)
-	sleep(1)
+web_dir = os.path.join(os.path.dirname(__file__), 'web')
+os.chdir(web_dir)
 
-	return p
-
-def print_url():
-
-	try:
-		print bool(urllib2.urlopen('http://127.0.0.1:8124').read())
-	except Exception as e:
-		print e
-
-print_url()
-p = serv()
-print_url()
-p.terminate()
-print_url()
-p = serv()
-print_url()
-p.terminate()
-print_url()
-p = serv()
-print_url()
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
