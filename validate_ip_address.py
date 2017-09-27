1446b8926375257ccf83b6322ea453133444228c
bharshbarger <bharshbarger@users.noreply.github.com>
2017-09-26 23:21:28 -0500

    Update validate_ip_address.py

--- validate_ip_address.py
+++ validate_ip_address.py
@@ -1,9 +1,11 @@
 import re
 
-ip_address_regex=re.compile("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}")
 
-if ip_address_regex.match(t):
-    return True
-else
-    return False
+def validate_ip_address(self,ip):
+    ip_address_regex=re.compile("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}")
+
+    if ip_address_regex.match(ip):
+        return True
+    else
+        return False
 
