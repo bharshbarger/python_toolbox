241bd4cf611d3bfd9f5d590a8f25a5e403e49522
bharshbarger <bharshbarger@users.noreply.github.com>
2017-09-27 21:19:41 -0500

    Update sigint_sigterm.py

--- sigint_sigterm.py
+++ sigint_sigterm.py
@@ -1,7 +1,21 @@
-def sigterm_handler(signal, frame):
-	#<functions>
+import signal
+import time
 
-def sigint_handler(signal, frame):
-	print('\nCaught Ctrl+C')
-	print('Press Enter to close')
-	#<functions>
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+
+if __name__ == '__main__':
+  killer = GracefulKiller()
+  while True:
+    time.sleep(1)
+    print("doing something in a loop ...")
+    if killer.kill_now:
+      break
+
+  print "End of the program. I was killed gracefully :)"
